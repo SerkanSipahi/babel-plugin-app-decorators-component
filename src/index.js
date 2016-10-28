96e6a00fdd2dbf3a6e9d857bc5abd16623302e64
Serkan Sipahi <serkan.sipahi@yahoo.de>
2016-10-29 00:29:17 +0200

    src/index.js: refactor

--- src/index.js
+++ src/index.js
@@ -1,97 +1,95 @@
 import { elements } from './elements';
 
+let getDecorator = function(decoratorName) {
+
+    let decorators = this.decorators || [];
+
+    for(let decorator of decorators){
+        let { name } = decorator.expression.callee;
+        if(decoratorName === name){
+            return decorator;
+        }
+    }
+
+    return null;
+};
+
+let getElementClassByName = function(name, elements) {
+
+    let element = elements[name] || 'HTMLElement';
+    return element;
+};
+
+let getArguments = function(t){
+
+    // set default
+    let args = {
+        extends: 'div',
+    };
+    let [ firstArgument ] = this.expression.arguments;
+
+    if(!t.isObjectExpression(firstArgument)){
+        return args;
+    }
+
+    let { properties } = firstArgument;
+    for(let property of properties){
+        args[property.key.name] = property.value.value
+    }
+
+    return args;
+};
+
+
+let addSuperClass = function(element, t) {
+
+    let identifier = t.identifier(element);
+    this.superClass = identifier;
+};
+
+let addStaticGetterProperty = function(type, superClass, t) {
+
+    let x = t.classMethod(
+        'get',
+        t.identifier(type),
+        [],
+        t.blockStatement([
+            t.returnStatement(t.stringLiteral(superClass))
+        ]),
+        false,
+        true
+    );
+
+    this.body.body.push(x);
+};
+
 /**
  * plugin
- * @astexplorer: https://astexplorer.net/#/DG3oIHIg5f/11
+ * @astexplorer: https://astexplorer.net
  * @param t
- * @returns {{visitor: {ClassDeclaration: (function(*))}}}
+ * @returns {{visitor: {ClassDeclaration: (function(*=))}}}
  */
 function plugin({types: t}) {
 
-    let getDecorator = (node, decoratorName) => {
-
-        let decorators = node.decorators || [];
-
-        for(let decorator of decorators){
-            let { name } = decorator.expression.callee;
-            if(decoratorName === name){
-                return decorator;
-            }
-        }
-
-        return null;
-    };
-
-    let getElementClassByName = (name, elements) => {
-
-        let element = elements[name] || 'HTMLElement';
-        return element;
-    };
-
-    let getArguments = (node) => {
-
-        // set default
-        let args = {
-            extends: 'div',
-        };
-        let [ firstArgument ] = node.expression.arguments;
-
-        if(!t.isObjectExpression(firstArgument)){
-            return args;
-        }
-
-        let { properties } = firstArgument;
-        for(let property of properties){
-            args[property.key.name] = property.value.value
-        }
-
-        return args;
-    };
-
-
-    let addSuperClass = (node, element) => {
-
-        let identifier = t.identifier(element);
-        node.superClass = identifier;
-    };
-
-    let addStaticGetterProperty = (node, type, superClass) => {
-
-        let x = t.classMethod(
-            'get',
-            t.identifier(type),
-            [],
-            t.blockStatement([
-                t.returnStatement(t.stringLiteral(superClass))
-            ]),
-            false,
-            true
-        );
-
-        node.body.body.push(x);
-    };
-
     return {
         visitor: {
-            ClassDeclaration(path) {
+            ClassDeclaration({ node } = path) {
 
-                let node = path.node;
-
-                let component = getDecorator(node, 'component');
+                let component = node::getDecorator('component');
                 if(!component){
                     return;
                 }
 
-                let superClass = getArguments(component).extends;
+                let superClass = component::getArguments(t).extends;
 
                 let element = getElementClassByName(superClass, elements);
-                addSuperClass(node, element);
+                node::addSuperClass(element, t);
 
                 if(superClass === 'div'){
                     return;
                 }
 
-                addStaticGetterProperty(node, 'extends', superClass)
+                node::addStaticGetterProperty('extends', superClass, t);
             },
         },
     };
